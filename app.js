ad4cea8a6ae83d4200d221b03af56bc1f7c5a45a
josh231101 <wiijosue333@gmail.com>
2020-07-04 13:31:06 -0500

    Mongoose add

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-//jshint esversion:6
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const date = require( __dirname + "/date.js");
 const app = express();
 
@@ -8,28 +8,67 @@ app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
 
-const newTasks = ["Buy some food","Eat the food."]
-const workItems = ["Dont stop"]
+//Connect to todolistDB
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser : true});
+
+//First _schema_ then _model_
+const itemsSchema = mongoose.Schema({
+  name : String
+})
+
+const Item = mongoose.model("Item",itemsSchema);
+
+const work = new Item({
+  name : "Work 3 hours"
+});
+const sleep = new Item({
+  name : "Sleep at 10"
+});
+const code = new Item({
+  name : "Code 10 hours"
+});
 
 app.get("/",function(req,res){
   const day = date.getDay();
 
-  res.render("lists",{
-    listTitle : day,
-    newListItems : newTasks,
-    route : "/"
-  });
-
+  Item.find(function(e,items){
+    if(items.length === 0){
+      Item.insertMany([sleep,code,work],function(err){
+        if(err){console.log(err);}
+        else {console.log("Array saved");}
+      });
+      res.redirect("/")
+    }else{
+      res.render("lists",{
+        listTitle : day,
+        newListItems : items,
+        route : "/"
+      })
+    }
+  })
 });
 
 app.post("/",function(req,res){
-  const newTask = req.body.task;
-  newTasks.push(newTask);
+  const itemName = req.body.task;
+  const newItem = new Item({
+    name : itemName
+  });
+  newItem.save();
+
   res.redirect("/")
-  console.log(newTask);
 });
 
+app.post("/delete",function(req,res){
+  const itemChecked = req.body.checkbox;
+  console.log(req.body.checkbox);
+  Item.deleteOne({_id: itemChecked},function(err){
+    if(err){console.log(err);}
+    else{res.redirect("/")}
+  })
+  //OTHER WAY
+  // Item.findByIdAndRemove(itemChecked,function(err){...})
 
+})
 app.get("/work",function(req,res){
   res.render("lists",{
     listTitle : "WorkList",
